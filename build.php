04d5c97427ccf3b999c2d4f1e62a64aed4ee8f6d
matsuo@tambourine.inc <matsuo@tam-bourine.co.jp>
2018-08-21 10:55:31 +0900

    init

---
+++ build.php
@@ -0,0 +1,14 @@
+<?php
+include "vendor/autoload.php";
+
+use Cake\Chronos\Chronos;
+
+$php_version = phpversion();
+$now = Chronos::now()->format('c');
+
+$html = <<< EOF
+    <p>version: {$php_version}</p>
+    <p>{$now}</p>
+EOF;
+
+file_put_contents('public/index.html', $html);
