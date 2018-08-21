77c4a8189764ecb3a3cab13fa18f9d981a1163f0
matsuoshi <matsuoshi@gmail.com>
2018-08-21 11:37:23 +0900

    Update build.php

--- build.php
+++ build.php
@@ -3,12 +3,12 @@ include "vendor/autoload.php";
 
 use Cake\Chronos\Chronos;
 
-$php_version = phpversion();
 $now = Chronos::now()->format('c');
+$php_version = phpversion();
 
 $html = <<< EOF
-    <p>version: {$php_version}</p>
     <p>now: {$now}</p>
+    <p>version: {$php_version}</p>
 EOF;
 
 file_put_contents('public/index.html', $html);
